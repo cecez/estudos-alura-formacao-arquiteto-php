d5885f2373e07d86476c3f05824feee392505997
Cezar Castro Rosa <cecez86@gmail.com>
2022-02-14 11:05:04 -0300

    refactor: renomeando atributo visible para published e removendo nulidade de atributo ageLimit

--- 003-alura-object-calisthenics-exercitando-a-orientacao-a-objetos/1957-object-calisthenics-projeto-inicial/src/Domain/Video/Video.php
+++ 003-alura-object-calisthenics-exercitando-a-orientacao-a-objetos/1957-object-calisthenics-projeto-inicial/src/Domain/Video/Video.php
@@ -4,26 +4,26 @@ namespace Alura\Calisthenics\Domain\Video;
 
 class Video
 {
-    private bool $visible = false;
-    private ?int $ageLimit;
+    private bool $published = false;
+    private int $ageLimit;
 
-    public function __construct(int $ageLimit = null)
+    public function __construct(int $ageLimit = 0)
     {
         $this->ageLimit = $ageLimit;
     }
 
     public function isPublished(): bool
     {
-        return $this->visible;
+        return $this->published;
     }
 
-    public function ageLimit(): ?int
+    public function ageLimit(): int
     {
         return $this->ageLimit;
     }
 
     public function publish()
     {
-        $this->visible = true;
+        $this->published = true;
     }
 }
